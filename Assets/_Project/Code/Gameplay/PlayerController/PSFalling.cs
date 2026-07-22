c438e83b5c6941721cba7616ad401dba4019fc6b
Yuki Little <nevan.little@hotmail.com>
2026-07-22 13:50:12 -0600

    PlayerStateMachine architecture finished
    now it just needs to be filled out

---
+++ Assets/_Project/Code/Gameplay/PlayerController/PSFalling.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class PSFalling : IState
+{
+    private PlayerController _player;
+    
+    public PSFalling(PlayerController player)
+    {
+        _player = player;
+    }
+    // if the player presses space go into mist form
+    // if the player hits the ground they should either go into walk or idle depending on if their di is 0 or not
+    // if the player hits a victim, suck their blood
+    public void Enter()
+    {
+        
+    }
+
+    public void Execute()
+    {
+
+    }
+
+    public void Exit()
+    {
+
+    }
+}
--- Assets/_Project/Code/Gameplay/PlayerController/PSMIst.cs
+++ Assets/_Project/Code/Gameplay/PlayerController/PSMIst.cs
@@ -8,7 +8,13 @@ public class PSMist : IState
     {
         _player = player;
     }
-    // 
+    // the inbetween dashing and transforming state
+    // give the player a bunch of speed in the direction they were going before (or not if from idle state)
+    // do a boxcast to check for victims and if there are go into blood sucking state
+    // if the player is still holding space by the end, put the player into bat state
+    // if the player stops holding space put them back into falling state
+    // if the player alternatively enters from the bat state the player should be put into falling (this may just happen anyway because they are not still holding space)
+    // there may need to be a timer to prevent the player from spamming this ability
     public void Enter()
     {
         
