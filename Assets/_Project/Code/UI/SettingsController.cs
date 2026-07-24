a5d0d32941bfe473a7dbb3ba039c451928b8b5af
Michael Hardwicke <michael.hardwicke92@gmail.com>
2026-07-24 15:30:15 -0600

    Updated Scenes w/ UI prefabs
    Thanks Marina!

--- Assets/_Project/Code/UI/SettingsController.cs
+++ Assets/_Project/Code/UI/SettingsController.cs
@@ -3,11 +3,7 @@ using UnityEngine.SceneManagement;
 
 namespace _Project.Code.UI
 {
-    /// <summary>
-    /// Placeholder options scene. For now it is just a Back button that returns to the menu.
-    /// Fill in volume/fullscreen/etc. later; keep the Back wiring when you do.
-    /// </summary>
-    public class OptionsController : MonoBehaviour
+    public class SettingsController : MonoBehaviour
     {
         [Tooltip("Scene to return to (the title). Must be in Build Settings.")]
         [SerializeField] private string _returnSceneName = "MainMenu";
